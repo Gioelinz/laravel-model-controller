b736821a54479fd9e85e8ec90a53b1c7add68e62
Gioelinz <gioelerunza.gr@gmail.com>
2022-03-17 15:29:55 +0100

    add header

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,12 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ env('APP_NAME') }}</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
 <body>
-    <header>
-        <h1>Test</h1>
-    </header>
+    @include('includes.header')
+
+    @yield('content')
 </body>
 
 </html>
